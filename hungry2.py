816c982238ba686584098305a6464ddbfcd9c3d0
Rima-sawant <sawant.reema@gmail.com>
2024-10-14 16:53:22 -0500

    added yo

--- hungry2.py
+++ hungry2.py
@@ -1,10 +1,11 @@
-hungry=input("are you hungry?")
-if hungry=="yes":
+hungry = input("are you hungry?")
+if hungry == "yes" and hungry == "yo":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
+    print("eat fries")
 else:
-    thirsty=input("are you thirsty?")
-    if thirsty=="yes":
+    thirsty = input("are you thirsty?")
+    if thirsty == "yes":
         print("drink water")
         print("drink soda")
